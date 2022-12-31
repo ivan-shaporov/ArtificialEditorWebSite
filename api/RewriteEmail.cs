b12c4c7b41c4cff643bba0f82a8912b8fd80bccd
Ivan Shaporov <ivan_shaporov@yahoo.com>
2022-12-30 19:16:41 -0800

    Add Az f().

---
+++ api/RewriteEmail.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Editor
+{
+    public static class RewriteEmail
+    {
+        [FunctionName("RewriteEmail")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            /*dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string name = data?.name;*/
+
+            string responseMessage = requestBody;
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
