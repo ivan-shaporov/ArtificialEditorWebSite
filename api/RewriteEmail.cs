e239041a6f4f0fc0f8b43e9d300fcf2dc5f1763e
Ivan Shaporov <ivan_shaporov@yahoo.com>
2023-01-10 18:57:12 -0800

    Report problems.

--- api/RewriteEmail.cs
+++ api/RewriteEmail.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -15,7 +16,7 @@ namespace Editor
     public static class RewriteEmail
     {
         [FunctionName("RewriteEmail")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> RunRewriteEmail(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
@@ -52,24 +53,74 @@ namespace Editor
             var completion = await client.GenerateCompletion(prefix.Replace("\\n", "\n") + request.Text);
             //var completion = client.GenerateCompletionStub(request.Text);
 
-            await StoreRewriteLog(prefix, request, completion);
+            string id = Hash(completion.Id);
+            string partition = DateTime.UtcNow.ToString("yyyy-MM-dd");
+
+            await StoreRewriteLog(partition, id, prefix, request, completion);
 
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            return new OkObjectResult(new { Text = completion.Text });
+            return new OkObjectResult(new { Text = completion.Text, Id = id });
         }
 
-        private static async Task StoreRewriteLog(string prefix, RewriteRequest request, Completion completion)
+        [FunctionName("ReportProblem")]
+        public static async Task<IActionResult> RunReportProblem(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            int maxRequestTextLength = int.Parse(Environment.GetEnvironmentVariable("MaxRequestTextLength"));
+            var overlength = 100;
+            var maxRequestLength = maxRequestTextLength * 11 + overlength;
+            var buffer = new char[maxRequestLength];
+            int length = await new StreamReader(req.Body).ReadAsync(buffer, 0, maxRequestTextLength + overlength);
+            
+            if (length >= maxRequestLength)
+            {
+                return new BadRequestResult();
+            }
+
+            var request = JsonConvert.DeserializeObject<ReportProblemRequest>(new string(buffer));
+            
+            if (request == null)
+            {
+                log.LogWarning("request is null.");
+                return new BadRequestResult();
+            }
+            
+            if (request.Text.Length > maxRequestTextLength ||
+                request.Rewritten.Length > maxRequestTextLength * 10)
+            {
+                return new BadRequestResult();
+            }
+
+            var client = await GetTableClient();
+            var entity = new TableEntity(request.Partition, request.Id);
+            entity.Add("ProblemInput", request.Text);
+            entity.Add("Problemoutput", request.Rewritten);
+
+            await client.UpdateEntityAsync(entity, Azure.ETag.All);
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkResult();
+        }
+
+        private static async Task<TableClient> GetTableClient()
         {
             var storageConnectionString = Environment.GetEnvironmentVariable("StorageConnectionString");
-
             var client = new TableClient(storageConnectionString, "rewrites");
-
             await client.CreateIfNotExistsAsync();
+            return client;
+        }
 
-            var entity = new TableEntity(partitionKey: DateTime.UtcNow.ToString("yyyy-MM-dd"), rowKey: completion.Id)
+        private static async Task StoreRewriteLog(string partitionKey, string rowKey, string prefix, RewriteRequest request, Completion completion)
+        {
+            var client = await GetTableClient();
+
+            var entity = new TableEntity(partitionKey, rowKey)
             {
                 { "Prefix", prefix },
+                { "CompletionId", completion.Id },
                 { "RequestTextLength", request.Text.Length },
                 { "PromptTokens", completion.PromptTokens },
                 { "CompletionTokens", completion.CompletionTokens },
@@ -84,11 +135,21 @@ namespace Editor
 
             await client.UpsertEntityAsync(entity);
         }
+
+        private static string Hash(string v) => $"{v.GetHashCode()}{new string(v.Reverse().ToArray()).GetHashCode()}";
     }
 
     class RewriteRequest
     {
         public string Text { get; set; }
         public bool AllowLog { get; set; }
+    }
+
+    class ReportProblemRequest
+    {
+        public string Partition { get; set; }
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Rewritten { get; set; }
     }
 }
