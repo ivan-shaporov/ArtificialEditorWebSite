8281b487840a3b936d620f70aa2caf210efe860e
Ivan Shaporov <ivan_shaporov@yahoo.com>
2023-01-15 09:06:37 -0800

    restore api folder

---
+++ api/OpenApiClient.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenApi
+{
+    public class OpenApiClient
+    {
+        public int MaxTokens { get; set; } = 256;
+        public Uri Url { get; set; } = new Uri("https://api.openai.com/v1/completions");
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public OpenApiClient(string key) => 
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
+
+        public Completion GenerateCompletionStub(string prompt) => new Completion(null)
+        {
+            Id = "stub", 
+            Text = prompt
+        };
+
+        public async Task<Completion> GenerateCompletion(string prompt)
+        {
+            var request = new
+            {
+                model = "text-davinci-003",
+#pragma warning disable IDE0037 // Use inferred member name
+                prompt = prompt,
+#pragma warning restore IDE0037 // Use inferred member name
+                max_tokens = MaxTokens,
+                temperature = 0.7
+            };
+
+            string requestBody = JsonConvert.SerializeObject(request);
+
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            var reply = await client.PostAsync(Url, content);
+
+            reply.EnsureSuccessStatusCode();
+
+            var jsonString = await reply.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<OpenApiResponse>(jsonString);
+
+            var result = new Completion(response);
+
+            return result;
+        }
+
+        internal class OpenApiResponse
+        {
+            public string Id { get; set; } //"cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7",
+            public string Object { get; set; } // "text_completion",
+            public ulong Created { get; set; } //  1589478378
+            public string Model { get; set; } //"text-davinci-003"
+            public OpenApiChoice[] Choices { get; set; }
+            public OpenApiUsage Usage { get; set; }
+        }
+
+        internal class OpenApiChoice
+        {
+            public string Text { get; set; } // "\n\nThis is indeed a test",
+            public int Index { get; set; } // 0,
+            public string Logprobs { get; set; } // null,
+            public string Finish_reason { get; set; } // "length"
+        }
+
+        internal class OpenApiUsage
+        {
+            public int Prompt_tokens { get; set; } // 5,
+            public int Completion_tokens { get; set; } // 7,
+            public int Total_tokens { get; set; } // 12
+        }
+    }
+    public class Completion
+    {
+        internal Completion(OpenApiClient.OpenApiResponse response)
+        {
+            Id = response?.Id;
+            Text = response?.Choices.FirstOrDefault()?.Text;
+            PromptTokens = response?.Usage.Prompt_tokens ?? 0;
+            CompletionTokens = response?.Usage.Completion_tokens ?? 0;
+            TotalTokens = response?.Usage.Total_tokens ?? 0;
+        }
+
+        public string Id { get; set; } //"cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7"
+        public string Text { get; set; } // "\n\nThis is indeed a test"
+        public int PromptTokens { get; set; } // 5,
+        public int CompletionTokens { get; set; } // 7,
+        public int TotalTokens { get; set; } // 12
+    }
+}
---
+++ api/RewriteEmail.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Data.Tables;
+using OpenApi;
+
+namespace Editor
+{
+    public static class RewriteEmail
+    {
+        [FunctionName("RewriteEmail")]
+        public static async Task<IActionResult> RunRewriteEmail(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            int maxRequestTextLength = int.Parse(Environment.GetEnvironmentVariable("MaxRequestTextLength"));
+            var overlength = 100;
+            var buffer = new char[maxRequestTextLength + overlength];
+            int length = await new StreamReader(req.Body).ReadAsync(buffer, 0, maxRequestTextLength + overlength);
+            
+            if (length >= maxRequestTextLength + overlength)
+            {
+                return new OkObjectResult(new { Text = "Your request is too long. Try a shorter one." });
+            }
+
+            var request = JsonConvert.DeserializeObject<RewriteRequest>(new string(buffer));
+            
+            if (request == null)
+            {
+                log.LogWarning("request is null.");
+                return new BadRequestResult();
+            }
+            
+            if (request.Text.Length > maxRequestTextLength)
+            {
+                return new OkObjectResult(new { Text = "Your input is too long. Try a shorter one." });
+            }
+
+            var client = new OpenApiClient(Environment.GetEnvironmentVariable("OpenApiKey"))
+            {
+                MaxTokens = int.Parse(Environment.GetEnvironmentVariable("MaxCompletionTokens"))
+            };
+
+            string prefix = Environment.GetEnvironmentVariable("Prefix");
+
+            var completion = await client.GenerateCompletion(prefix.Replace("\\n", "\n") + request.Text);
+            //var completion = client.GenerateCompletionStub(request.Text);
+
+            string id = Hash(completion.Id);
+            string partition = DateTime.UtcNow.ToString("yyyy-MM-dd");
+
+            await StoreRewriteLog(partition, id, prefix, request, completion);
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult(new {Partition = partition, Id = id, Text = completion.Text });
+        }
+
+        [FunctionName("Ping")]
+        public static IActionResult RunPing(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkResult();
+        }
+
+        [FunctionName("ReportProblem")]
+        public static async Task<IActionResult> RunReportProblem(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            int maxRequestTextLength = int.Parse(Environment.GetEnvironmentVariable("MaxRequestTextLength"));
+            var overlength = 100;
+            var maxRequestLength = maxRequestTextLength * 11 + overlength;
+            var buffer = new char[maxRequestLength];
+            int length = await new StreamReader(req.Body).ReadAsync(buffer, 0, maxRequestTextLength + overlength);
+            
+            if (length >= maxRequestLength)
+            {
+                return new BadRequestResult();
+            }
+
+            var request = JsonConvert.DeserializeObject<ReportProblemRequest>(new string(buffer));
+            
+            if (request == null)
+            {
+                log.LogWarning("request is null.");
+                return new BadRequestResult();
+            }
+            
+            if (request.Text.Length > maxRequestTextLength ||
+                request.Rewritten.Length > maxRequestTextLength * 10)
+            {
+                return new BadRequestResult();
+            }
+
+            var client = await GetTableClient();
+            var entity = new TableEntity(request.Partition, request.Id);
+            entity.Add("ProblemInput", request.Text);
+            entity.Add("Problemoutput", request.Rewritten);
+
+            await client.UpdateEntityAsync(entity, Azure.ETag.All);
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkResult();
+        }
+
+        private static async Task<TableClient> GetTableClient()
+        {
+            var storageConnectionString = Environment.GetEnvironmentVariable("StorageConnectionString");
+            var client = new TableClient(storageConnectionString, "rewrites");
+            await client.CreateIfNotExistsAsync();
+            return client;
+        }
+
+        private static async Task StoreRewriteLog(string partitionKey, string rowKey, string prefix, RewriteRequest request, Completion completion)
+        {
+            var client = await GetTableClient();
+
+            var entity = new TableEntity(partitionKey, rowKey)
+            {
+                { "Prefix", prefix },
+                { "CompletionId", completion.Id },
+                { "RequestTextLength", request.Text.Length },
+                { "PromptTokens", completion.PromptTokens },
+                { "CompletionTokens", completion.CompletionTokens },
+                { "TotalTokens", completion.TotalTokens },
+            };
+
+            if (request.AllowLog)
+            {
+                entity["input"] = request.Text;
+                entity["output"] = completion.Text;
+            }
+
+            await client.UpsertEntityAsync(entity);
+        }
+
+        private static string Hash(string v) => $"{v.GetHashCode()}{new string(v.Reverse().ToArray()).GetHashCode()}";
+    }
+
+    class RewriteRequest
+    {
+        public string Text { get; set; }
+        public bool AllowLog { get; set; }
+    }
+
+    class ReportProblemRequest
+    {
+        public string Partition { get; set; }
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Rewritten { get; set; }
+    }
+}
