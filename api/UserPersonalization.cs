57f862a2facd1fe19f9889264e5da3a2a96a3a10
Ivan Shaporov <ivan_shaporov@yahoo.com>
2023-01-21 14:36:32 -0800

    log UserPersonalization api request method

--- api/UserPersonalization.cs
+++ api/UserPersonalization.cs
@@ -43,6 +43,8 @@ namespace Editor
 
             string method = req.Method.ToLowerInvariant();
 
+            log.LogInformation($"UserPersonalization request {method}");
+
             if (method == "get")
             {
                 return await GetUserPersonalization(table, userId);
@@ -51,10 +53,10 @@ namespace Editor
             {
                 return await SaveUserPersonalization(req, table, userId);
             }
-            /*else if (method == "delete")
+            else if (method == "delete")
             {
                 return await DeleteUserPersonalization(table, userId);
-            }*/
+            }
             else
             {
                 return new BadRequestObjectResult($"Method '{req.Method}' not supported");
